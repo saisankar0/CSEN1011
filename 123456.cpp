bba0c15ea39c3485d97a7616796250b3ca3ceab8
saisankar0 <93367691+saisankar0@users.noreply.github.com>
2021-11-19 12:23:50 +0530

    Add files via upload

---
+++ 123456.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z;
+	printf("enter a value for x");
+	scanf("%d",&x);
+	printf("enter a value for y");
+	scanf("%d",&y);
+	z=x+y;
+	printf("the sum of the two numbers is z: %d",z);
+	
+	return(0);
+}
