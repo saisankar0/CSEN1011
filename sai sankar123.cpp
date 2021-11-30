2c3e3a697342b10eaaaecf021eb804633f77a3aa
saisankar0 <93367691+saisankar0@users.noreply.github.com>
2021-11-30 14:44:54 +0530

    Add files via upload

---
+++ sai sankar123.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float num,divisor,remainder;
+	
+	printf("enter the number:");
+	scanf("%f",&divisor);
+	
+	while(num>=divisor);
+	{
+		num = num-divisor;
+	}
+	  remainder=num;
+	  printf("the remainder is =%f",remainder);
+}
