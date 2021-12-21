accb097d3e53fb1d4cbecf2bb15b2e32308adf4b
saisankar0 <93367691+saisankar0@users.noreply.github.com>
2021-12-21 14:29:47 +0530

    Add files via upload

---
+++ floyds triangle.123.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include <conio.h>  
+void main()  
+{  
+    int num, i, j, k = 1;   
+   
+    printf( " Enter a number : \n");  
+    scanf( "%d", &num);  
+    
+    for (i = 1; i <= num; i++)  
+    {  
+        
+        for (j = 1; j <= i; j++)  
+        {  
+            printf(" %2d", k++); 
+        }  
+        printf( "\n");  
+        }     
+        getch();   
+}  
